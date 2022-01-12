b851e9b28fad1bb89840515e750a887fd0808164
Jakub Antoniak <jakub.antoniak@equicty.com>
2022-01-12 23:01:01 +0100

    command reader refactor

--- src/main/java/com/corneliusz/bank/CommandReader.java
+++ src/main/java/com/corneliusz/bank/CommandReader.java
@@ -13,60 +13,70 @@ public class CommandReader {
         if (firstCommand.isEmpty()) {
             return;
         }
-
-
-//        int i = 0;
-
-//
-//        while (i < commandSplit.length) {
-//            String nextValue = commandSplit[i];
-//            System.out.println(nextValue);
-//            i++;
-//        }
-
         if (commandSplit[0].equals("login")) {
-            String loginInput = commandSplit[1];
-            String passwordInput = commandSplit[2];
-            if (loginInput.equals(login) && passwordInput.equals(haslo)) {
-                zalogowany = true;
-                System.out.println("Zalogowales sie mordo!");
-            } else {
-                System.out.println("Mordo pojebales login i/lub haslo");
-            }
+            this.obluzLogin(commandSplit);
             return;
         }
-
         if (!zalogowany) {
             System.out.println("Musisz sie najpierw zalogowac");
             return;
         }
+        obslugaKomendDlaZalogowanych(commandSplit, firstCommand);
+    }
 
+    public void obsluzWyplate(String[] wpis) {
+        int valueOut = Integer.parseInt(wpis[1]);
+        if (valueOut <= saldo) {
+            saldo -= valueOut;
+            System.out.println("wyplacono " + valueOut + "€. Twoje saldo wynosi: " + saldo + "€.");
+        } else {
+            System.out.println("Nie stac cie na ta operacje");
+        }
+    }
+
+    public void obsluzWplate(String[] pis) {
+        int valueIn = Integer.parseInt(pis[1]);
+        saldo += valueIn;
+        System.out.println("wplacono " + valueIn + "€. Twoje saldo wynosi: " + saldo + "€.");
+    }
+
+    public void obsluzSaldo() {
+        System.out.println("Twoje saldo wynosi: " + saldo + "€");
+    }
+
+    public void obsluzLogout() {
+        zalogowany = false;
+        System.out.println("Zostales poprawnie wylogowany");
+    }
+
+    public void obluzLogin(String[] wpis) {
+        String loginInput = wpis[1];
+        String passwordInput = wpis[2];
+        if (loginInput.equals(login) && passwordInput.equals(haslo)) {
+            zalogowany = true;
+            System.out.println("Zalogowales sie mordo!");
+        } else {
+            System.out.println("Mordo pojebales login i/lub haslo");
+        }
+    }
+
+    public void obslugaKomendDlaZalogowanych(String[] commandSplit, String firstCommand) {
         switch (firstCommand) {
             case "logout":
-                zalogowany = false;
-                System.out.println("Zostales poprawnie wylogowany");
+                this.obsluzLogout();
                 break;
             case "wplac":
-                int valueIn = Integer.parseInt(commandSplit[1]);
-                saldo += valueIn;
-                System.out.println("wplacono " + valueIn + "€. Twoje saldo wynosi: " + saldo + "€.");
+                this.obsluzWplate(commandSplit);
                 break;
             case "wyplac":
-                int valueOut = Integer.parseInt(commandSplit[1]);
-                if (valueOut <= saldo) {
-                    saldo -= valueOut;
-                    System.out.println("wyplacono " + valueOut + "€. Twoje saldo wynosi: " + saldo + "€.");
-                } else {
-                    System.out.println("Nie stac cie na ta operacje");
-                }
+                this.obsluzWyplate(commandSplit);
                 break;
             case "saldo":
-                System.out.println("Twoje saldo wynosi: " + saldo + "€");
+                this.obsluzSaldo();
                 break;
             default:
                 System.out.println("Wpisz poprawna komende");
                 break;
         }
     }
-
 }
